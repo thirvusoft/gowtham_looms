1599866e606a779d43f2842cc2bd4d3464f1002f
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-09-01 12:38:42 +0530

    Chore: Naming Series Updated

--- gowtham_looms/gowtham_looms/custom/py/item_group.py
+++ gowtham_looms/gowtham_looms/custom/py/item_group.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 
 def validate(self,action):
     company = frappe.db.get_single_value("Global Defaults","default_company")
@@ -8,7 +9,5 @@ def validate(self,action):
     doc = frappe.db.sql (f""" select * from tabSeries where name = "{naming_series}" """)
     if len(doc)==0:
         update_query = frappe.db.sql(f""" INSERT INTO tabSeries VALUES ("{naming_series}",0) """)
-        
-
-    
-    
+        old_series = frappe.get_meta('Item').get_field("naming_series").options
+        make_property_setter('Item', 'naming_series', 'options', f'{old_series}\n{naming_series}', 'Text Editor')
