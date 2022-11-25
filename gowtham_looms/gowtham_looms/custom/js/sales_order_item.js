df7ec663a63aab486db284e35e015d84108f9dbf
root <root@ip-172-26-0-174.ap-south-1.compute.internal>
2022-11-25 11:05:37 +0000

    Print format

--- gowtham_looms/gowtham_looms/custom/js/sales_order_item.js
+++ gowtham_looms/gowtham_looms/custom/js/sales_order_item.js
@@ -1,7 +1,9 @@
 frappe.ui.form.on("Sales Order Item", {
     item_code: function(frm, cdt, cdn){
         var row = locals[cdt][cdn]
-        frappe.db.get_list('Item Variant Attribute', {filters:{parentfield:"attributes",parenttype:"Item",parent:row.item_code}, fields:['attribute_value']}).then((message) => {
+        if(row.attribute == "Width")
+        frappe.db.get_list('Item Variant Attribute', {filters:{parentfield:"attributes",parenttype:"Item",parent:row.item_code,attribute:"Width"}, fields:['attribute_value']}).then((message) => {
+            
             frappe.model.set_value(cdt, cdn,'ts_size',message[0].attribute_value);
         })
         
