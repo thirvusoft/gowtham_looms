91c909baa5d0c4487e741460585d1020761b2d77
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-11-25 15:10:38 +0530

    chore:Sales Invoice and Sales Order

---
+++ gowtham_looms/gowtham_looms/custom/js/sales_invoice_item.js
@@ -0,0 +1,9 @@
+frappe.ui.form.on("Sales Invoice Item", {
+    item_code: function(frm, cdt, cdn){
+        var row = locals[cdt][cdn]
+        frappe.db.get_list('Item Variant Attribute', {filters:{parentfield:"attributes",parenttype:"Item",parent:row.item_code}, fields:['attribute_value']}).then((message) => {
+            frappe.model.set_value(cdt, cdn,'ts_size',message[0].attribute_value);
+        })
+        
+    },
+})
