e0e13fc9b03dca725d170faf23aa1678f93e4d00
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-11-29 09:45:31 +0530

    Chore: Raw Material Naming Series

--- gowtham_looms/gowtham_looms/custom/py/item.py
+++ gowtham_looms/gowtham_looms/custom/py/item.py
@@ -26,12 +26,14 @@ def create_service_item():
 def validate(doc, actions):
         abbr = f'{doc.abbrevation}.##.-.{doc.ts_type}'
         old_series = frappe.get_meta('Item').get_field("naming_series").options.split()
-        if(doc.item_group != "Looms"):
+        if(doc.ts_type not in ["",None]):
             if(abbr not in old_series):
                 update_query = frappe.db.sql(f""" INSERT INTO tabSeries VALUES ("{abbr}",0) """)
                 old_series = frappe.get_meta('Item').get_field("naming_series").options
                 defaults = frappe.get_meta('Item').get_field("naming_series").default
                 make_property_setter('Item', 'naming_series', 'options', f'{old_series}\n{abbr}', 'Text Editor')
                 make_property_setter('Item', 'naming_series', 'default', defaults, 'Text Editor')
+        elif(doc.item_group != "Looms"):
+            doc.name = make_autoname("R"+'-'+ doc.abbrevation + '.##')
         else:
             doc.name = make_autoname(doc.item_name)
